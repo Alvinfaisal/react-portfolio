4e89bc6b53aa6f9464545b52a4baa3ef18920f68
Alvinfaisal <faisalvin15@gmail.com>
2021-09-09 18:52:01 +0700

    styling error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,22 @@
 const Error = () => {
-    return (
-        <>
-            <h1>Error 404. Page Not Found</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <section className="error" style={{ 
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column"
+        }}>
+        <h1>Error 404. Page Not Found</h1>
+        <Link
+          to="/"
+          style={{ backgroundColor: "#00b392", padding: 10, color: "white" }}
+        >
+          Homepage
+        </Link>
+      </section>
+    </>
+  );
+};
 
-export default Error
+export default Error;
