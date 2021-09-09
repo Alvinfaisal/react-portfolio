3151613c07a15c6607d3fbf85de3fea6b26fb6ae
Alvinfaisal <faisalvin15@gmail.com>
2021-09-09 18:57:54 +0700

    nothing simple

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <>
-      <section className="error" style={{ 
+      <section
+        className="error"
+        style={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          flexDirection: "column"
-        }}>
+          flexDirection: "column",
+        }}
+      >
         <h1>Error 404. Page Not Found</h1>
         <Link
           to="/"
